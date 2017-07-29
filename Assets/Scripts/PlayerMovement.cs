2445b68e713a1573584577cb5e899f7fb6713b5e
AJ Weeks <ajweeks@shaw.ca>
2017-07-29 10:30:55 +0200

    Add basic player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,14 +2,32 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject Child;
+
+    public float MovementSpeed;
+
+    //private Vector2 _targetVelocity;
+    private Rigidbody _rb;
+
 
 	void Start ()
     {
-		
+        _rb = GetComponent<Rigidbody>();
 	}
 	
 	void Update ()
     {
-		
-	}
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (horizontalInput != 0.0f)
+        {
+            _rb.AddForce(new Vector3(MovementSpeed * horizontalInput, 0, 0), ForceMode.Impulse);
+        }
+
+        if (verticalInput != 0.0f)
+        {
+            _rb.AddForce(new Vector3(0, 0, MovementSpeed * verticalInput), ForceMode.Impulse);
+        }
+    }
 }
