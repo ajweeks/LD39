4bc483ce6ca865e61d658aad240d49d570475366
AJ Weeks <ajweeks@shaw.ca>
2017-07-30 14:45:56 +0200

    Add power bar sprites
    Fix battery rotation locks

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,19 +7,16 @@ public class UIManager : MonoBehaviour
 
     public Gradient PowerBarColor;
 
-    private float _powerBarStartWidth;
     private PowerManager _playerPowerManager;
 
     private void Start()
     {
         _playerPowerManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PowerManager>();
-        _powerBarStartWidth = PowerBarImage.rectTransform.sizeDelta.x;
     }
 
     void Update () 
 	{
-        RectTransform powerBarRT = PowerBarImage.rectTransform;
-        powerBarRT.sizeDelta = new Vector2(_playerPowerManager.PowerLevel * _powerBarStartWidth, powerBarRT.sizeDelta.y);
+        PowerBarImage.fillAmount = _playerPowerManager.PowerLevel;
         PowerBarImage.color = PowerBarColor.Evaluate(_playerPowerManager.PowerLevel);
     }
 }
